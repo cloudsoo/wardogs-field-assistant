8a762a32e5717b74c2fc9a7f2de58fab43f9f280
cloudsoo <143735896+cloudsoo@users.noreply.github.com>
2026-09-09 03:26:05 +0800

    Bump PWA cache to V9

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='wardogs-field-v8';
-const CORE=['./','./index.html','./app.js','./app-v8.js','./manifest.json','./icon.svg'];
+const CACHE='wardogs-field-v9';
+const CORE=['./','./index.html','./app.js','./app-v9.js','./app-map-v9.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(e.request.method==='GET'&&res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return res}))))
