2b5d84e612d6122a7dd278602aa0b4bb7b3dffce
cloudsoo <143735896+cloudsoo@users.noreply.github.com>
2026-09-09 04:23:47 +0800

    chore: rotate service worker cache for V13 map

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='wardogs-field-v12';
-const CORE=['./','./index.html','./app.js','./app-v11.js','./map-v12-fix.js','./manifest.json','./icon.svg'];
+const CACHE='wardogs-field-v13';
+const CORE=['./','./index.html','./app.js','./app-v11.js','./map-v13-fix.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
